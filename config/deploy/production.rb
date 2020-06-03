a1103c51f3efb6561a71b1669b5d3fdfe57a53a9
Bill Harding <wbharding@gmail.com>
2020-06-03 13:04:37 -0700

    Don't ignore cap production config file >:( First stab at a production cap file

---
+++ config/deploy/production.rb
@@ -0,0 +1,28 @@
+set :stage, :app
+set :rails_env, "production"
+
+Airbrussh.configure do |config|
+  config.log_file = "log/deploy_app.log"
+end
+
+server 'misc1.bonanza.com', user: 'deployuser', roles: %w(app web), primary: true
+
+# if ENV["SILENT"] != "true"
+#   before "deploy:starting", "slack:notify_start"
+#   after "deploy:finished", "slack:notify_end"
+# end
+
+# after :deploy, "passenger:conditional_restart"
+
+# Wbh June 2020: taken from original file
+# Simple Role Syntax
+# ==================
+# Supports bulk-adding hosts to roles, the primary server in each group
+# is considered to be the first unless any hosts have the primary
+# property set.  Don't declare `role :all`, it's a meta role.
+
+# role :app, %w(deploy@example.com)
+# role :web, %w(deploy@example.com)
+# role :db,  %w(deploy@example.com)
+
+
